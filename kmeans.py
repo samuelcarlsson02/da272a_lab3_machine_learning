5ea6e98ed189eacfb56552a92bd23ad9280c192c
samuelcarlsson02 <samuel.carlsson4@gmail.com>
2024-12-18 11:25:50 +0100

    started working

--- kmeans.py
+++ kmeans.py
@@ -36,8 +36,29 @@ def distance(a,b):
 #
 #  
 def kmeans(data_points, num_clusters, termination_tol, max_iter):
-    
-    # You should implement the kmeans algorithm by editing this function.
+    centroids = rd.sample(data_points, num_clusters)
+    cluster_assignments = [0] * len(data_points)
+
+    for iteration in range(max_iter):
+        for i, point in enumerate(data_points):
+            distances = [distance(point, centroid) for centroid in centroids]
+            cluster_assignments[i] = distances.index(min(distances))
+        
+        new_centroids = []
+        for cluster_idx in range(num_clusters):
+            cluster_points = [data_points[i] for i in range(len(data_points)) if cluster_assignments[i] == cluster_idx]
+            if cluster_points:
+                new_centroid = [sum(dim) / len(cluster_points) for dim in zip(*cluster_points)]
+                new_centroids.append(new_centroid)
+            else:
+                new_centroids.append(rd.choice(data_points))
+        centroid_shifts = [distance(new_centroids[i], centroids[i]) for i in range(num_clusters)]
+        if max(centroid_shifts) < termination_tol:
+            print(f"Coverged after {iteration + 1} iterations.")
+            break
+        centroids = new_centroids
+    return centroids, cluster_assignments
+
 
 
 
@@ -56,14 +77,15 @@ max_iter = 100
 fig, ax = plt.subplots()
 ax.scatter(data_points['x'], data_points['y'], c='black')
 plt.title("Data points")
-plt.show()
+# plt.show()
 
 
 num_clusters_to_test = 15
 total_dist_elbow = []
 
 for k in range(1,num_clusters_to_test+1):
-    kmeans_output = kmeans(data_points, k, termination_tol, max_iter)
+    data_points_list = data_points[['x', 'y']].values.tolist()
+    kmeans_output = kmeans(data_points_list, k, termination_tol, max_iter)
     total_dist_elbow.append(kmeans_output[2])
     
 #Plot elbow curve
